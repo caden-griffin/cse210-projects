98311e63158af413285d7ea70fb07b0fa07101d5
caden-griffin <cadengriffin0@gmail.com>
2026-01-08 18:52:11 -0700

    Completed Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+
+        
+        int guess = -1384234252;
+
+        while (guess != magicNumber)
+        {
+            Console.WriteLine("What is the magic number?");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (magicNumber == guess)
+            {
+                Console.WriteLine($"Correct! You guessed {guess}, which is the magic number!");
+            }
+        }
+        
     }
 }
