54a7c0893e63fb48c33e2f85b68ac7f1067c59d5
caden-griffin <cadengriffin0@gmail.com>
2026-01-08 21:23:32 -0700

    completed exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int number = -1;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (number != 0)
+        {
+            
+            string userNumber = Console.ReadLine();
+            number = int.Parse(userNumber);
+
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+
+        }
+        
+    int sum = 0;
+    foreach (int listNumber in numbers)
+    {
+        sum += listNumber;
     }
+
+    float averageListNumber = 0;
+    averageListNumber = (float)sum / numbers.Count;
+
+    int largestNumber = 0;
+    foreach (int listNumber in numbers)
+        {
+            if (listNumber > largestNumber)
+            {
+                largestNumber = listNumber;
+            }
+        }
+
+    Console.WriteLine($"The sum is {sum}");
+    Console.WriteLine($"The average is {averageListNumber}");
+    Console.WriteLine($"The largest number is {largestNumber}");
+            
+    }
+
 }
